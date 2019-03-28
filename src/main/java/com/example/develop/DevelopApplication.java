093e20f77c09e1514aeae08e3fbb74ef9f17e59c
guohuixie2@creditease.cn <guohuixie2@creditease.cn>
2019-03-28 18:32:51 +0800

    oo

--- src/main/java/com/example/develop/DevelopApplication.java
+++ src/main/java/com/example/develop/DevelopApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DevelopApplication {
 
     public static void main(String[] args) {
+        //+++
         SpringApplication.run(DevelopApplication.class, args);
     }
 }
